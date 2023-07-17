5c613c8f518f35ce8445b980af882dced8233fce
Arjunsingh Rajput <asr9320003120@gmail.com>
2023-07-18 01:16:05 +0530

    fix:  sidebar header

--- src/Components/Music/Music.js
+++ src/Components/Music/Music.js
@@ -3,7 +3,7 @@ import { BsPlayCircle } from "react-icons/bs";
 import { IoPlayBack, IoPlayForward } from "react-icons/io5";
 import { HiMiniSpeakerWave } from "react-icons/hi2";
 import { FaPauseCircle } from "react-icons/fa";
-import {FaCirclePlay} from "react-icons/fa6"
+import { FaCirclePlay } from "react-icons/fa6";
 import { BsThreeDots } from "react-icons/bs";
 import { useData } from "../../Context/DataContext";
 import "./Music.css";
@@ -14,9 +14,7 @@ export const Music = () => {
     audioRef,
   } = useData();
   const [isPlaying, setIsPlaying] = useState(false);
-  const [progress, setProgress] = useState(50); 
-
-
+  const [progress, setProgress] = useState(50);
 
   const handlePlayPause = () => {
     if (isPlaying) {
@@ -33,57 +31,60 @@ export const Music = () => {
       audioRef.current.load();
       // audioRef.current.play();
       setIsPlaying(true);
-      
-     
     }
   }, [selectedSong, playing]);
 
   return (
-    <div className="main-music-container">
-    <div>
-
-
-      <div className="song-details heading">
-        <h2 className="song-title-heading">{selectedSong?.title}</h2>
-        <p className="song-artist-heading">{selectedSong?.artist}</p>
-      </div>
-      <div className="img-box">
-        <img 
-          src={selectedSong?.photo}
-          className="selectedSong-img"
-          alt="song-img"
- 
-        />
-      </div>
-      <div className="music-seeker">
-      <div className="seeker-bar">
-        <div className="progress-bar" style={{ width: `${progress}%` }}></div>
-      </div>
-    </div>
-      <div className="more-play-mute-btn">
+    <div className="main-menu-box">
+      <div className="main-music-container">
         <div>
-          <button className="react-icons-btns round-circle">
-            <BsThreeDots />
-          </button>
+          <div className="song-details heading">
+            <h2 className="song-title-heading">{selectedSong?.title}</h2>
+            <p className="song-artist-heading">{selectedSong?.artist}</p>
+          </div>
+          <div className="img-box">
+            <img
+              src={selectedSong?.photo}
+              className="selectedSong-img"
+              alt="song-img"
+            />
+          </div>
+          <div className="music-seeker">
+            <div className="seeker-bar">
+              <div
+                className="progress-bar"
+                style={{ width: `${progress}%` }}
+              ></div>
+            </div>
+          </div>
+          <div className="more-play-mute-btn">
+            <div>
+              <button className="react-icons-btns round-circle">
+                <BsThreeDots />
+              </button>
+            </div>
+            <div className="back-play-next-btn">
+              <button className="react-icons-btns react-icons-btns-size-2  ">
+                <IoPlayBack />
+              </button>
+              <audio ref={audioRef} src={selectedSong?.url} />
+              <button
+                className="react-icons-btns react-icons-btns-size "
+                onClick={handlePlayPause}
+              >
+                {isPlaying ? <FaPauseCircle /> : <FaCirclePlay />}
+              </button>
+              <button className="react-icons-btns react-icons-btns-size-2 ">
+                <IoPlayForward />
+              </button>
+            </div>
+            <div>
+              <button className="react-icons-btns round-circle">
+                <HiMiniSpeakerWave />
+              </button>
+            </div>
+          </div>
         </div>
-        <div className="back-play-next-btn">
-          <button className="react-icons-btns react-icons-btns-size-2  ">
-            <IoPlayBack />
-          </button>
-          <audio ref={audioRef} src={selectedSong?.url} />
-          <button className="react-icons-btns react-icons-btns-size " onClick={handlePlayPause}>
-            {isPlaying ? <FaPauseCircle /> : <FaCirclePlay />}
-          </button>
-          <button className="react-icons-btns react-icons-btns-size-2 ">
-            <IoPlayForward />
-          </button>
-        </div>
-        <div>
-          <button className="react-icons-btns round-circle">
-            <HiMiniSpeakerWave />
-          </button>
-        </div>
-      </div>
       </div>
     </div>
   );
